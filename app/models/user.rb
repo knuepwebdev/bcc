6864bfbf4bdf5adad6880d87facf2d798bc868ad
Mike <mikeknueppel@hotmail.com>
2016-02-28 19:10:11 -0800

    Update a users visited apps, update the page with visited apps listed first and highlighted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps  
+
+  has_many :visited_apps
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  field :apps_visited, type: Array
   ## Database authenticatable
   field :email,              type: String, default: ""
   field :encrypted_password, type: String, default: ""

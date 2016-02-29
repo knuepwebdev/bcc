367d4448a09fd8ba71516536de3db0b0483657d4
Mike <mikeknueppel@hotmail.com>
2016-02-28 21:21:52 -0800

    update url

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 VisitedApp.destroy_all
 App.destroy_all
 App.create(name: 'above_top_secret', urls: ['http://www.abovetopsecret.com/ats.js'])
-App.create(name: 'youtube', urls: ['https://s0.2mdn.net/ads/richmedia/studio/pv2/41658857/20160225180354493/styles.css'])
+App.create(name: 'youtube', urls: ['https://www.gstatic.com/doubleclick/studio/innovation/h5/ytplayer/ytp_v2.js'])
 App.create(name: 'ebay', urls: ['http://ir.ebaystatic.com/rs/v/jmalt0eyvq1k1k2ezqntv51k5mo.js'])
 App.create(name: 'myspace', urls: ['https://x.myspacecdn.com/new/common/js/global.E96CEE97EE0DAD8555F138732D741DFB.min.js'])
 App.create(name: 'expedia', urls: ['https://a.travel-assets.com/uitoolkit/2-54/fbcb5d1ca30b588acd9869323c65940a244c6abf/core/minifiedCss/brands/exp-responsive.css'])
